67615bb507f3b39b5f03593518049ab0d3c7b646
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2025-09-16 10:14:32 +0530

    feat: implement logout

--- src/app/buyers/page.tsx
+++ src/app/buyers/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { LeadsTable } from "@/components/leads-table";
+import { Button } from "@/components/ui/button";
 import { useUser } from "@/context/user-context";
-import { Loader2 } from "lucide-react";
+import { useFetchApi } from "@/hooks/use-fetch";
+import { Loader2, UserMinus } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const BuyersPage = () => {
-  const { userId, loading: userLoading } = useUser();
+  const { userId, loading: userLoading, setUserId } = useUser();
   const router = useRouter();
+  const fetchApi = useFetchApi();
   useEffect(() => {
     if (userLoading) return;
     if (!userId) {
@@ -30,17 +33,36 @@ const BuyersPage = () => {
     return null;
   }
 
+  const handleLogout = async () => {
+    setUserId(null);
+    router.push("/");
+    await fetchApi("/api/auth/logout", {
+      method: "POST",
+    });
+  };
+
   return (
     <div className="container mx-auto py-8 px-4">
-      <div className="mb-8">
-        <h1 className="text-3xl font-bold text-balance">
-          Property Leads Management
-        </h1>
-        <p className="text-muted-foreground mt-2">
-          Manage and track your property leads with advanced filtering and
-          search capabilities.
-        </p>
+      <div className="flex items-start justify-between">
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold text-balance">
+            Property Leads Management
+          </h1>
+          <p className="text-muted-foreground mt-2">
+            Manage and track your property leads with advanced filtering and
+            search capabilities.
+          </p>
+        </div>
+        <Button
+          variant="destructive"
+          className="flex items-center gap-2"
+          onClick={handleLogout}
+        >
+          <UserMinus className="w-4 h-4" />
+          Logout
+        </Button>
       </div>
+
       <LeadsTable />
     </div>
   );
